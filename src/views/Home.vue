010057a4270954a11b1e34821acad4afa7e705d3
stan <stan@stans-mbp.home>
2019-04-02 21:53:37 +0200

    create movie view and rename default component to moviesTable

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MoviesTable/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import MoviesTable from '@/components/MoviesTable.vue';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
+    MoviesTable,
   },
 };
 </script>
--- src/views/Movie.vue
+++ src/views/Movie.vue
@@ -1,6 +1,38 @@
 <template>
   <div class="movie">
     <h1>This is a movie page</h1>
-    <h2>id: {{ $route.params.id }}</h2>
+    <div v-if="film">
+      <p>title: {{ film.title }}</p>
+      <p>director: {{ film.director }}</p>
+      <p>producer: {{ film.producer }}</p>
+      <p>release date: {{ film.release_date }}</p>
+      <p>opening crawl: {{ film.opening_crawl }}</p>
+    </div>
+    <p v-if="loading">Loading...</p>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'Movie',
+  data() {
+    return {
+      film: null,
+      loading: false,
+    };
+  },
+  mounted() {
+    this.loading = true;
+    this.$axios.get(`https://swapi.co/api/films/${this.$route.params.id}`)
+      .then((res) => {
+        this.film = res.data;
+        console.log(this.film);
+        this.loading = false;
+      })
+      .catch((error) => {
+        console.error(error);
+        this.loading = false;
+      });
+  },
+};
+</script>
