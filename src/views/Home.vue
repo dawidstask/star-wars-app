4393c7c562ca215ead1d9baf466a1c68cf17a413
stan <stan@stans-mbp.home>
2019-04-03 23:04:45 +0200

    fix router and add logo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <img alt="Star Wars logo" src="../assets/star-wars-logo.png">
     <MoviesTable/>
   </div>
 </template>
