f5aa40c747e8720c062ba3966dc08517d8dd1684
stan <stan@stans-mbp.home>
2019-04-03 22:20:09 +0200

    Initial dist subtree commit

---
+++ precache-manifest.8c0bdbdd13e5ede580df25c5a64ef504.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "97229456d2641f619f05",
+    "url": "/<star-wars-app>/js/movie.e68d6444.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/<star-wars-app>/robots.txt"
+  },
+  {
+    "revision": "1e289350c8b74140798b",
+    "url": "/<star-wars-app>/js/reviews.001a8e58.js"
+  },
+  {
+    "revision": "76c835369972c80d8de5",
+    "url": "/<star-wars-app>/js/chunk-vendors.47f91812.js"
+  },
+  {
+    "revision": "7aabe97d6fa1a19e049c",
+    "url": "/<star-wars-app>/js/app.9fdf5ccf.js"
+  },
+  {
+    "revision": "23f1e624d09fda038413425c1a5bc497",
+    "url": "/<star-wars-app>/index.html"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "/<star-wars-app>/fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "/<star-wars-app>/fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "97229456d2641f619f05",
+    "url": "/<star-wars-app>/css/movie.cd1f7cad.css"
+  },
+  {
+    "revision": "76c835369972c80d8de5",
+    "url": "/<star-wars-app>/css/chunk-vendors.84040707.css"
+  },
+  {
+    "revision": "7aabe97d6fa1a19e049c",
+    "url": "/<star-wars-app>/css/app.af9b3352.css"
+  }
+];
